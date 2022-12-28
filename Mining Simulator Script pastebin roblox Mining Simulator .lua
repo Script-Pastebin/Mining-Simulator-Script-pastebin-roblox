d1bb1daabcb2d5ee31947f8369fb204ea61e8a02
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:52:15 +0100

    commit zero

---
+++ Mining Simulator Script pastebin roblox Mining Simulator .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet(("https://pastebin.com/raw/p4ihNBSw"),true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Functional script for one of the most popular Mining Simulator games
+						
+]]
